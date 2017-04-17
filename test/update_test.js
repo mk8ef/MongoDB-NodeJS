2a38ca383a6905b47dff60e43cc7ef06b04208ee
Marko Krkeljas <marko.krkeljas@ACE.APEXCLEANENERGY.COM>
2017-04-17 16:51:41 -0400

    update

--- test/update_test.js
+++ test/update_test.js
@@ -6,7 +6,7 @@ describe('Updating Records', () => {
     let joe;
 
     beforeEach((done)=> {
-        joe = new User({name: 'Joe', postCount: 0});
+        joe = new User({name: 'Joe', likes: 0});
         joe.save()
            .then(() => done());
     });
@@ -87,10 +87,10 @@ describe('Updating Records', () => {
     it('Increment user point count by 1', () => {
     // Class based update - so working on the entire group
     // Use mongo operators instead of looping through the collection
-        User.update({name: 'Joe', }, {$inc: {postCount: 1}})
+        User.update({name: 'Joe', }, {$inc: {likes: 10}})
             .then(() => User.findOne({name: 'Joe'}))
             .then((user) => {
-                assert(user.postCount === 1)
+                assert(user.likes === 1)
             })
     })
     
